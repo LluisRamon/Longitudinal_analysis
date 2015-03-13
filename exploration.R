366d2354f2f632dc8abab1640ff7120ae71eaf43
Lluis Ramon <lluisramon.tgn@gmail.com>
2015-03-13 17:43:39 +0100

    Simplify exploration code

--- exploration.R
+++ exploration.R
@@ -5,20 +5,12 @@ cows <- read.table("data/cattle_mes dades.txt", header = TRUE,
 
 names(cows) <- c("id", "dose", "pcv", "time", "nbirth")
 cows$dose <- factor(cows$dose, levels = c("L", "M", "H"))
-
 str(cows)
 head(cows)
+
+# Summary statistics ------------------------------------------------------
+
 summary(cows)
-
-table(cows$time)
-table(cows$nbirth)
-table(cows$nbirth, cows$id)
-
-table(cows$id)
-
-# There are 9 mesures for each id
-# TODO: In de .doc says there are 5 cows but there are 10 id and with different
-# doses. I don't understand data.
 
 boxplot(pcv ~ time, data = cows)
 boxplot(pcv ~ dose, data = cows)
@@ -30,12 +22,9 @@ library(ggplot2) # Package for graphics
 
 qplot(nbirth, pcv, data = cows, facets = .~ dose, colour = factor(time))
 
-
-# Posible graphs, but I am not sure about data structure
 qplot(time, pcv, data = cows, group = id, geom = "line", facets = .~ dose) 
 qplot(time, pcv, data = cows, group = id, geom = "line", facets = .~ dose, 
       colour = nbirth) 
-
 
 # Add a linear model ------------------------------------------------------
 
